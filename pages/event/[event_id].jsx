490878d827d2591369f50049ab8881f91e5cce29
Emre Aydin <aydin@desap.co.uk>
2021-03-08 20:08:53 +0000

    Adding untracked files

---
+++ pages/event/[event_id].jsx
@@ -0,0 +1,189 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useList, usePresence } from '@roomservice/react';
+import {
+  Box, Center, SimpleGrid, Text, Tabs, Button, Tab, TabPanel, TabPanels, TabList, Select,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react';
+import { Line } from 'react-chartjs-2';
+import { FaPlus } from 'react-icons/fa';
+import {
+  CgSmileMouthOpen, CgSmile, CgSmileNeutral, CgSmileSad,
+} from 'react-icons/cg';
+import { BiSad } from 'react-icons/bi';
+import { GoAlert } from 'react-icons/go';
+
+import Layout from '../../components/Layout';
+import Question from '../../components/Question';
+
+const placeholderQuestion = {
+  type: 'input',
+  label: '',
+  placeholder: '',
+  optionGroup: [''],
+  sliderGroup: ['', '', ''],
+  helper: '',
+  required: false,
+  answer: '',
+};
+
+export default function Home() {
+  const [selectedMood, setSelectedMood] = useState();
+  const [tabs, setTabs] = useState(['1']);
+  const [techIssue, setTechIssue] = useState();
+  const [techIssueModal, setTechIssueModal] = useState(false);
+
+  const [surveysList, setSurveysList] = useList('event', 'surveys');
+  const [techIssuesList, setTechIssuesList] = useList('event', 'techIssues');
+  const [moodList, setMoodList] = useList('event', 'mood');
+
+  const [joined, joinedClient] = usePresence('event', 'joined');
+
+  const [template, setTemplate] = useState({
+    title: '',
+    questions: [
+      { ...placeholderQuestion },
+    ],
+  });
+
+  const onSendFeedback = async () => {
+    if (!setSurveysList) return;
+    let feedbackText = '';
+    for (let i = 0; i < template.questions.length; i++) {
+      feedbackText += `${template.questions[i].answer}. `;
+    }
+    try {
+      const res = await axios.post('/api/v1/feedback', {
+        feedback: feedbackText,
+      });
+      const result = res.data;
+      console.log(result);
+      setMood(mood + result);
+      // ! send the mood/result (not sure which is which) to roomservice
+      // setMoodList.push(?);
+    } catch (err) {
+      console.log('failed');
+    }
+    setSurveysList.push(template);
+  };
+
+  useEffect(() => {
+    joinedClient.set(true);
+  }, []);
+
+  const sendMood = async (moodScore) => {
+    setSelectedMood(moodScore);
+    setMoodList.push(moodScore);
+  };
+
+  return (
+    <Layout title="Event">
+      <Center width="100vw" height="50px" background="#2D3748">
+        <Text
+          bgGradient="linear(to-l, #7928CA,#FF0080)"
+          bgClip="text"
+          fontSize="35px"
+          fontWeight="extrabold"
+          textAlign="center"
+        >
+          WWFeedback
+        </Text>
+      </Center>
+      <Box margin="10px" borderRadius="lg" bg="#2D3748" padding="10px">
+        <Center>
+          <Text
+            bgGradient="linear(to-l, #7928CA,#FF0080)"
+            bgClip="text"
+            fontSize="25px"
+            fontWeight="extrabold"
+            textAlign="center"
+          >
+            How do you feel about this event?
+          </Text>
+        </Center>
+        <Center>
+          <CgSmileMouthOpen color="lime" fontSize="100px" style={{ cursor: 'pointer', opacity: [undefined, 1].includes(selectedMood) ? 1 : 0.3 }} onClick={() => sendMood(1)} />
+          <CgSmile color="#66ff66" fontSize="100px" style={{ cursor: 'pointer', opacity: [undefined, 0.75].includes(selectedMood) ? 1 : 0.3 }} onClick={() => sendMood(0.75)} />
+          <CgSmileNeutral color="yellow" fontSize="100px" style={{ cursor: 'pointer', opacity: [undefined, 0.5].includes(selectedMood) ? 1 : 0.3 }} onClick={() => sendMood(0.5)} />
+          <CgSmileSad color="#ff3300" fontSize="100px" style={{ cursor: 'pointer', opacity: [undefined, 0.25].includes(selectedMood) ? 1 : 0.3 }} onClick={() => sendMood(0.25)} />
+          <BiSad color="red" fontSize="100px" style={{ cursor: 'pointer', opacity: [undefined, 0].includes(selectedMood) ? 1 : 0.3 }} onClick={() => sendMood(0)} />
+        </Center>
+      </Box>
+      <SimpleGrid columns={{ sm: 1, lg: 2 }} spacing="20px">
+        <Box margin="10px" borderRadius="lg" bg="#2D3748" padding="10px">
+          <Text
+            bgGradient="linear(to-l, #7928CA,#FF0080)"
+            bgClip="text"
+            fontSize="25px"
+            fontWeight="extrabold"
+          >
+            Surveys
+          </Text>
+          <Tabs>
+            <TabList>
+              {tabs.map((tab) => <Tab>{tab}</Tab>)}
+            </TabList>
+
+            <TabPanels>
+              {tabs.map(() => (
+                <TabPanel>
+                  {template?.questions?.map((question, questionNo) => (<Question template={template} setTemplate={setTemplate} question={question} questionNo={questionNo} />))}
+                  <Button onClick={onSendFeedback}>
+                    Send Feedback
+                  </Button>
+                </TabPanel>
+              ))}
+            </TabPanels>
+          </Tabs>
+        </Box>
+        <Box margin="10px">
+          <Button leftIcon={<GoAlert />} colorScheme="red" onClick={() => setTechIssueModal(true)}>
+            Report Technical Issue
+          </Button>
+          <Modal isOpen={techIssueModal} onClose={() => setTechIssueModal(false)}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Report A Technical Issue</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Select onChange={(e) => setTechIssue(e.target.value)} placeholder="Select technical issue" size="lg">
+                  <option value="Host microphone muted/not working">Host microphone muted/not working</option>
+                  <option value="Presentation not visible">Presentation not visible</option>
+                  <option value="No audible sound">No audible sound</option>
+                </Select>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button disabled={!techIssue} colorScheme="blue" mr={3} onClick={() => setTechIssuesList.push(techIssue)}>
+                  Send Issue
+                </Button>
+                <Button variant="outline" onClick={() => setTechIssueModal(false)}>Cancel</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </Box>
+      </SimpleGrid>
+    </Layout>
+  );
+}
+
+// export async function getStaticPaths(context) {
+//   // const res = await axios.get('/api/v1/sessions', {
+//   //   params: {
+//   //     id: code,
+//   //   },
+//   // });
+
+//   return {
+//     paths: [
+//       { params: { } }, // See the "paths" section below
+//     ],
+//     fallback: false, // See the "fallback" section below
+//   };
+// }
---
+++ pages/host/[host_id].jsx
@@ -0,0 +1,224 @@
+import { useState, useEffect, useRef } from 'react';
+import {
+  Box, Center, SimpleGrid, Text, useToast,
+} from '@chakra-ui/react';
+import { useList, usePresence } from '@roomservice/react';
+import { Line } from 'react-chartjs-2';
+import { VscSmiley } from 'react-icons/vsc';
+
+import {
+  CgSmileMouthOpen, CgSmile, CgSmileNeutral, CgSmileSad,
+} from 'react-icons/cg';
+import { BiSad } from 'react-icons/bi';
+
+import Layout from '../../components/Layout';
+
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
+
+export default function Home() {
+  const toast = useToast();
+
+  const [surveysList, setSurveysList] = useList('event', 'surveys');
+  const [techIssuesList, setTechIssuesList] = useList('event', 'techIssues');
+  const [moodList, setMoodList] = useList('event', 'mood');
+
+  const [mood, setMood] = useState(0.5);
+
+  const [joined, joinedClient] = usePresence('event', 'joined');
+
+  const data = {
+    labels: ['-10', '-9', '-8', '-7', '-6', '-5', '-4', '-3', '-2', '-1'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+      },
+    ],
+  };
+
+  useInterval(() => {
+    if (moodList.length > 0) {
+      // ! do mood stuff here
+      console.log(moodList);
+      setMood(moodList[0]); // ? takes last result, not average
+      setMoodList.delete(0);
+    }
+    if (techIssuesList.length > 0) {
+      toast({
+        title: (<>
+          An attendee has reported the following:
+          <br />
+          <i>{techIssuesList[0]}</i>
+        </>),
+        status: 'error',
+        isClosable: true,
+        duration: null,
+      });
+      setTechIssuesList.delete(0);
+    }
+  }, 2000);
+
+  useEffect(() => {
+    joinedClient.set(true);
+  }, []);
+
+  return (
+    <Layout title="Host View">
+      <Center width="100vw" height="50px" background="#2D3748">
+        <Text
+          bgGradient="linear(to-l, #7928CA,#FF0080)"
+          bgClip="text"
+          fontSize="35px"
+          fontWeight="extrabold"
+          textAlign="center"
+        >
+          WWFeedback
+        </Text>
+      </Center>
+      <SimpleGrid columns={[1, null, 2]} spacing="40px" margin="25px">
+        <SimpleGrid columns={[1, null, 2]} spacing="40px">
+
+          <Box borderRadius="lg" bg="#2D3748" minWidth="300px" padding="10px">
+            <Center>
+              <Text
+                bgGradient="linear(to-l, #7928CA,#FF0080)"
+                bgClip="text"
+                fontSize="25px"
+                fontWeight="extrabold"
+                textAlign="center"
+              >
+                Event Details
+              </Text>
+            </Center>
+            <b>Test event</b>
+            <br />
+            This is a test description about the test event for the Dragons' Den video.
+            <br />
+            <br />
+            <b>Attendees: </b>
+            {Object.keys(joined).length}
+          </Box>
+          <Box borderRadius="lg" bg="#2D3748" padding="10px">
+            <Center>
+              <Text
+                bgGradient="linear(to-l, #7928CA,#FF0080)"
+                bgClip="text"
+                fontSize="25px"
+                fontWeight="extrabold"
+                textAlign="center"
+              >
+                Mood Over Time
+              </Text>
+            </Center>
+            <Line height="200px" data={data} />
+          </Box>
+          <Box borderRadius="lg" bg="#2D3748" padding="10px">
+            <Center>
+              <Text
+                bgGradient="linear(to-l, #7928CA,#FF0080)"
+                bgClip="text"
+                fontSize="25px"
+                fontWeight="extrabold"
+                textAlign="center"
+              >
+                Attendees Over Time
+              </Text>
+            </Center>
+            <Line height="200px" data={data} />
+          </Box>
+          <Box borderRadius="lg" bg="#2D3748" padding="10px">
+            <Center>
+              <Text
+                bgGradient="linear(to-l, #7928CA,#FF0080)"
+                bgClip="text"
+                fontSize="25px"
+                fontWeight="extrabold"
+                textAlign="center"
+              >
+                Current Mood
+              </Text>
+            </Center>
+            <Center>
+              {mood >= 0.875 && (<CgSmileMouthOpen color="lime" fontSize="200px" />)}
+              {mood >= 0.625 && mood < 0.875 && (<CgSmile color="#66ff66" fontSize="200px" />)}
+              {mood >= 0.375 && mood < 0.625 && (<CgSmileNeutral color="yellow" fontSize="200px" />)}
+              {mood >= 0.125 && mood < 0.375 && (<CgSmileSad color="#ff3300" fontSize="200px" />)}
+              {mood < 0.125 && (<BiSad color="red" fontSize="200px" />)}
+            </Center>
+          </Box>
+        </SimpleGrid>
+        <Box height="calc(100vh - 100px)" style={{ overflowY: 'auto' }} borderRadius="lg" padding="10px" bg="#2D3748">
+          <Text
+            bgGradient="linear(to-l, #7928CA,#FF0080)"
+            bgClip="text"
+            fontSize="25px"
+            fontWeight="extrabold"
+          >
+            Feedback Panel
+          </Text>
+          {[].concat(surveysList).reverse().map((l) => (
+            <Box borderWidth="1px" marginBottom="10px" borderRadius="lg" overflow="hidden" padding="10px">
+              {l?.questions?.map((q) => (
+                <span>
+                  <b>{q.label}</b>
+                  :
+                  {' '}
+                  {q.answer}
+                  <br />
+                </span>
+              ))}
+            </Box>
+          ))}
+        </Box>
+      </SimpleGrid>
+    </Layout>
+  );
+}
+
+// export async function getStaticPaths(context) {
+//   // const res = await axios.get('/api/v1/sessions', {
+//   //   params: {
+//   //     id: code,
+//   //   },
+//   // });
+
+//   return {
+//     paths: [
+//       { params: { } }, // See the "paths" section below
+//     ],
+//     fallback: false, // See the "fallback" section below
+//   };
+// }
